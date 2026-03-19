f053a1767fc392fa1613b7ae78fc9218b5f90a79
ricardopatriciodossantos <ricardo.dossantos+amadeus@amadeus.com>
2026-03-19 15:43:45 +0000

    v2.6.141

---
+++ Framworks/UltralightFramework.xcframework/ios-arm64_x86_64-simulator/UltralightFramework.framework/Headers/UltralightFramework.h
@@ -0,0 +1,18 @@
+//
+//  UltralightFramework.h
+//  UltralightFramework
+//
+//  Created by ICM on 1/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UltralightFramework.
+FOUNDATION_EXPORT double UltralightFrameworkVersionNumber;
+
+//! Project version string for UltralightFramework.
+FOUNDATION_EXPORT const unsigned char UltralightFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UltralightFramework/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "UltralightFramework",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "UltralightFramework",
+            targets: ["UltralightFramework"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UltralightFramework",
+            path: "Framworks/UltralightFramework.xcframework"
+        )
+    ]
+)
